c98c9241cff71273cdd0611daad6522fa81067d1
danivivriti <danimvijay@gmail.com>
2019-05-09 21:41:53 +0530

    Updates

---
+++ precache-manifest.8a1f699cc8a901cd02ff6e37c99df417.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92f5be3f430fcf6ca746488f55fb0547",
+    "url": "/select-box-slug-generator/index.html"
+  },
+  {
+    "revision": "5dd15ab09711eef8db25",
+    "url": "/select-box-slug-generator/static/css/2.4223df75.chunk.css"
+  },
+  {
+    "revision": "bdcfd44a96980a310e51",
+    "url": "/select-box-slug-generator/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "5dd15ab09711eef8db25",
+    "url": "/select-box-slug-generator/static/js/2.c55027fd.chunk.js"
+  },
+  {
+    "revision": "bdcfd44a96980a310e51",
+    "url": "/select-box-slug-generator/static/js/main.d4f3e9db.chunk.js"
+  },
+  {
+    "revision": "f7a593a506d71d685df0",
+    "url": "/select-box-slug-generator/static/js/runtime~main.a3cd0d0c.js"
+  }
+]);
