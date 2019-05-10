ce4f6c26a88a28d29ef6a72389828bdf887fae41
danivivriti <danimvijay@gmail.com>
2019-05-10 12:05:57 +0530

    fix: add key array and hash to clear all

--- src/App.jsx
+++ src/App.jsx
@@ -39,6 +39,8 @@ const App = () => {
             setinput('');
             setoutputObj('');
             setoutputSelectboxVal('');
+            setoutputKeyArr('');
+            setoutputHash('');
       };
 
       return (
