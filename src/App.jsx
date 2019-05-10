b3cde0606a64b08ab89812f6722a679a98e4b097
danivivriti <danimvijay@gmail.com>
2019-05-10 11:02:36 +0530

    feat: add key array, hash, scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import React, { useState } from 'react';
-import 'bulma/css/bulma.css';
 import slug from 'slug';
-import { Columns, Column, Box, Label, Control, TextArea, Input, Field, Button, Title } from 'bloomer';
+import { Columns, Column, Box, Label, Control, TextArea, Input, Field, Button, Title, Icon } from 'bloomer';
+import { animateScroll as scroll } from 'react-scroll';
+
+import 'bulma/css/bulma.css';
 
 const App = () => {
       const [input, setinput] = useState('');
-      const [output, setoutput] = useState('');
-      const [selectboxVal, setselectboxVal] = useState('');
+      const [outputObj, setoutputObj] = useState('');
+      const [outputSelectboxVal, setoutputSelectboxVal] = useState('');
+      const [outputKeyArr, setoutputKeyArr] = useState('');
+      const [outputHash, setoutputHash] = useState('');
 
       const handleConvert = () => {
             const inputArr = input.split(',');
-            const outputArr = [];
+            const outputObjArr = [];
             const selectboxOptionsArr = [];
+            const outputKeyArrVals = [];
+            const outputHashArr = [];
             for (let item of inputArr) {
                   const key = slug(item, '_')
                         .toLowerCase()
@@ -19,24 +25,28 @@ const App = () => {
                   const value = item.trim();
                   const itemObj = { key, value };
                   selectboxOptionsArr.push(`<option value='${key}'>${value}</option>`);
-                  outputArr.push(itemObj);
+                  outputObjArr.push(itemObj);
+                  outputKeyArrVals.push(key);
+                  outputHashArr.push(`"${key}" => "${value}"`);
             }
-            setoutput(JSON.stringify(outputArr));
-            setselectboxVal(`<select name="">${selectboxOptionsArr.join('')}<select>`);
+            setoutputObj(JSON.stringify(outputObjArr));
+            setoutputSelectboxVal(`<select name="">${selectboxOptionsArr.join('')}<select>`);
+            setoutputKeyArr(JSON.stringify(outputKeyArrVals));
+            setoutputHash(`{ ${outputHashArr.join(', ')} }`);
       };
 
       const handleReset = () => {
             setinput('');
-            setoutput('');
-            setselectboxVal('');
+            setoutputObj('');
+            setoutputSelectboxVal('');
       };
 
       return (
             <div className="App">
                   <Columns isCentered>
-                        <Column isSize="1/3" style={{ marginTop: '10px' }}>
+                        <Column isSize="2/3" style={{ marginTop: '10px' }}>
                               <Box>
-                                    <Title>Selectbox Slug Generator</Title>
+                                    <Title>Array Converter</Title>
                                     <Field>
                                           <Label>Elements (separated by comma)</Label>
                                           <Control>
@@ -53,12 +63,23 @@ const App = () => {
                                                 Convert
                                           </Button>
                                     </Field>
+                                    <Field isGrouped>
+                                          <Button isColor="warning" isFullWidth onClick={handleReset}>
+                                                Reset
+                                          </Button>
+                                    </Field>
+                                    <Field>
+                                          <Label>Keys</Label>
+                                          <Control>
+                                                <Input type="text" placeholder={`["bread_butter","orange_milk"]`} value={outputKeyArr} />
+                                          </Control>
+                                    </Field>
                                     <Field>
                                           <Label>Object</Label>
                                           <Control>
                                                 <TextArea
-                                                      placeholder="[{ key: 'bread_butter', value: 'Bread Butter'}, { key: 'orange_milk', value: 'Orange Milk'}]"
-                                                      value={output}
+                                                      placeholder={`[{"key":"bread_butter","value":"Bread Butter"},{"key":"orange_milk","value":"Orange Milk"}]`}
+                                                      value={outputObj}
                                                       readOnly
                                                 />
                                           </Control>
@@ -68,17 +89,25 @@ const App = () => {
                                           <Control>
                                                 <TextArea
                                                       placeholder={`<select name=""><option value='bread_butter'>Bread Butter</option><option value='orange_milk'>Orange Milk</option><select>`}
-                                                      value={selectboxVal}
+                                                      value={outputSelectboxVal}
                                                       readOnly
                                                 />
                                           </Control>
                                     </Field>
-                                    <Field isGrouped>
-                                          <Button isColor="warning" isFullWidth onClick={handleReset}>
-                                                Reset
-                                          </Button>
+                                    <Field>
+                                          <Label>Hash</Label>
+                                          <Control>
+                                                <TextArea
+                                                      placeholder={`{ "bread_butter" => "Bread Butter", "orange_milk" => "Orange Milk" }`}
+                                                      value={outputHash}
+                                                      readOnly
+                                                />
+                                          </Control>
                                     </Field>
                               </Box>
+                              <Button isColor="success" isOutlined onClick={scroll.scrollToTop}>
+                                    <Icon className="fas fa-arrow-up" />
+                              </Button>
                         </Column>
                   </Columns>
             </div>
